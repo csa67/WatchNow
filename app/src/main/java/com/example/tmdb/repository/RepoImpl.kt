c6685af338738b9104884ef3e018fe949b13a276
csa67 <saiaarathy@gmail.com>
2025-04-11 07:28:05 -0400

    undoing suspend call of get query

--- app/src/main/java/com/example/tmdb/repository/RepoImpl.kt
+++ app/src/main/java/com/example/tmdb/repository/RepoImpl.kt
@@ -1,6 +1,7 @@
 package com.example.tmdb.repository
 
 import android.util.Log
+import com.example.tmdb.BuildConfig
 import com.example.tmdb.R
 import com.example.tmdb.db.FavDao
 import com.example.tmdb.model.FavMovie
@@ -14,7 +15,7 @@ class MovieRepoImpl(private val apiService: RestApiCollection):MovieRepoInterfac
     override suspend fun getPopularMovies(): PopularMovies? {
 
         return try{
-            apiService.getMovies("7bfb5935faa90f3b4913f80ed990bc15")
+            apiService.getMovies(BuildConfig.TMDB_API_KEY)
         }catch (e:Exception){
             Log.e("Error",e.stackTraceToString())
             null
